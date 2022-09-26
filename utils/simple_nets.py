291681ac27a03c27071037f009800f63484efc20
KezhiAdore <1538001851@qq.com>
2022-09-26 22:17:26 +0800

    :sparkles: implement MLP neural network

---
+++ utils/simple_nets.py
@@ -0,0 +1,31 @@
+from torch import nn
+
+
+class MLP(nn.Module):
+
+    def __init__(self, in_size, hidden_sizes, out_size):
+        super(MLP, self).__init__()
+        if isinstance(hidden_sizes, int):
+            hidden_sizes = [hidden_sizes]
+        self._layers = []
+        # hidden layers
+        for size in hidden_sizes:
+            self._layers.append(
+                nn.Sequential(
+                    nn.Linear(in_features=in_size, out_features=size),
+                    nn.ReLU()
+                )
+            )
+            in_size = size
+        # output layer
+        self._layers.append(
+            nn.Linear(in_features=in_size, out_features=out_size)
+        )
+        # transfer list to module list
+        self._layers = nn.ModuleList(self._layers)
+
+    def forward(self, input):
+        out = input
+        for layer in self._layers:
+            out = layer(out)
+        return out
