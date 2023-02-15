957b2bee743b23649b9d71c398e7c946d9e3a71d
KezhiAdore <1538001851@qq.com>
2023-02-15 15:43:35 +0800

    :sparkles: add simple `lunarlander` reward

--- test/reward.py
+++ test/reward.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+
 def cart_pole_v1(params):
     reward = params["reward"]
     step = params["step"]
@@ -9,6 +10,14 @@ def cart_pole_v1(params):
     else:
         return reward
 
+
+def lunar_lander_v2(params):
+    reward = params["reward"]
+    step = params["step"]
+    return reward - step * 0.05
+
+
 RewardFuncDict = defaultdict(lambda: None)
 RewardFuncDict["CartPole-v1"] = cart_pole_v1
 RewardFuncDict["CartPole-v0"] = cart_pole_v1
+RewardFuncDict["LunarLander-v2"] = lunar_lander_v2
